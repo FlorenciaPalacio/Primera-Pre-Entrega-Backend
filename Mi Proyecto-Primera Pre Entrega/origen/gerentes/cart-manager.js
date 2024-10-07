cce301f865cb2151459fb726a2653179f1b1d8f2
Florencia Palacio <137527381+FlorenciaPalacio@users.noreply.github.com>
2024-10-07 17:10:58 -0300

    Update cart-manager.js

--- Mi Proyecto-Primera Pre Entrega/origen/gerentes/cart-manager.js
+++ Mi Proyecto-Primera Pre Entrega/origen/gerentes/cart-manager.js
@@ -5,9 +5,7 @@ class CartManager {
     constructor(path) {
         this.path = path;
         this.carts = [];
-        this.ultId = 0;
-
-        //Cargar los carritos almacenados en el archivo: 
+        this.ultId = 0; 
         this.cargarCarritos();
     }
 
@@ -18,11 +16,9 @@ class CartManager {
             if (this.carts.length > 0) {
                 //Verifico si hay por lo menos algun elemento y voy a calcular el ultimo id: 
                 this.ultId = Math.max(...this.carts.map(cart => cart.id));
-                //Utilizo el método map para crear un nuevo array que solo tenga los ids y con Math.Max obtengo el mayor, guardandolo en la propiedad ultId. 
             }
         } catch (error) {
             console.log("Error al cargar el carrito");
-            //Si no existe el archivo, lo voy a crear: 
             await this.guardarCarritos();
         }
     }
@@ -31,18 +27,15 @@ class CartManager {
         await fs.promises.writeFile(this.path, JSON.stringify(this.carts, null, 2));
     }
 
-    //Metodo para crear un carrito: 
-
     async crearCarrito() {
         const nuevoCarrito = {
             id: ++this.ultId,
             products: []
         };
-
-        //Este objeto "carrito" lo pusheamos al array: 
+        
         this.carts.push(nuevoCarrito);
 
-        //Guardamos el array en el archivo: 
+       
         await this.guardarCarritos();
         return nuevoCarrito;
     }
@@ -66,7 +59,6 @@ class CartManager {
     async agregarProductoAlCarrito(carritoId, productoId, quantity = 1) {
         const carrito = await this.getCarritoById(carritoId);
         const existeProducto = carrito.products.find(p => p.product === productoId);
-        //De esta forma chequeo si el producto que estoy recibiendo para agregar al carrito ya esta presente en el. Si existe modifico la cantidad, si no existe lo agrego. 
 
         if (existeProducto) {
             existeProducto.quantity += quantity;
